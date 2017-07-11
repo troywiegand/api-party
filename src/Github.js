5cc1714df2e73f11f2e6817f8340524d13f4ff8e
Seth Baughman <sbaughman1@me.com>
2017-07-11 15:38:48 -0400

    Create github user component

--- src/Github.js
+++ src/Github.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Github.css'
 import { Route } from 'react-router-dom'
+import GithubUser from './GithubUser'
 
 class Github extends Component {
   state = {
@@ -37,7 +38,7 @@ class Github extends Component {
             <button type="submit">Look up Github user</button>
           </div>
         </form>
-        <Route path="/github/:username" render={(props) => <h3>You searched for {props.match.params.username} </h3>} />
+        <Route path="/github/:username" component={GithubUser} />
         <Route exact path="/github" render={() => <h3>Please enter a username to search on Github.</h3>} />
       </div>
     )
---
+++ src/GithubUser.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GithubUser extends Component {
+  render() {
+    return <h1>github user</h1>
+  }
+}
+
+export default GithubUser
