0bb6d4481e41e99f1bf0495f44506d21a078cc51
Seth Baughman <sbaughman1@me.com>
2017-07-11 15:16:00 -0400

    Add markup and styling for Github component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, NavLink } from 'react-router-dom'
+import Github from './Github'
 
 class App extends Component {
   render() {
@@ -18,7 +19,7 @@ class App extends Component {
           </ul>
         </div>
         <Switch>
-          <Route path='/github' render={() => <h1>Github!</h1>} />
+          <Route path='/github' component={Github} />
           <Route render={() => <p>To get started, click one of the links above</p>} />
         </Switch>
       </div>
---
+++ src/Github.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Github.css'
+
+class Github extends Component {
+  render() {
+    return (
+      <div className="github">
+        <img 
+          className="github-logo" 
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="github logo"
+        />
+        <form>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            <button type="submit">Look up Github user</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Github
