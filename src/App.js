8bc327746cf1ba5ebab9d51945fe363a62ac0dec
Troy <troy@troywiegand.com>
2018-07-10 11:41:18 -0400

    Add switch component with default route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 
 import './App.css';
 
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
       <header>
       <h3>It's a bop and a half</h3>
-        <h1> Ain't no party </h1>
+        <h1> API party </h1>
         
         </header>
       <ul className="navLinks">
@@ -18,7 +18,10 @@ class App extends Component {
         </li>
       </ul>
 
+      <Switch>
         <Route path="/github" render={()=> <h1>Github!</h1>}/>
+        <Route render={()=> <p>Click a h*ckin' link</p>}/>
+      </Switch>
       </div>
 
     
