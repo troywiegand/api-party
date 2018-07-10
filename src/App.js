7c2f93f86c32629845976671e062e63211f7973e
Troy <troy@troywiegand.com>
2018-07-10 16:45:06 -0400

    Add the HW Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, NavLink, Switch} from 'react-router-dom'
 
 import './App.css';
 import Github from './Github'
+import Homework from './Homework'
 
 class App extends Component {
   render() {
@@ -17,10 +18,14 @@ class App extends Component {
         <li>
           <NavLink to="/github"> GitHub API! </NavLink>
         </li>
+        <li>
+            <NavLink to="/homework">Homework</NavLink>
+          </li>
       </ul>
 
       <Switch>
         <Route path="/github" component={Github}/>
+        <Route path="/homework" component={Homework} />
         <Route render={()=> <p>Click a h*ckin' link</p>}/>
       </Switch>
       </div>
