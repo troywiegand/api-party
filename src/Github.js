7ab126a7b2398e4a608c22a76073add64eb5a09a
Troy <troy@troywiegand.com>
2018-07-10 11:45:21 -0400

    Added Github component and put in the switch in App

---
+++ src/Github.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+
+class Github extends Component {
+    render(){
+        return(
+            <div className="Github" >
+            Component
+            </div>
+        )
+    }
+}
+
+export default Github
