2e9820187b6011853b5e1f570e78a82541de4b55
Troy <troy@troywiegand.com>
2018-07-10 11:53:18 -0400

    Add Github style and made form controlled

--- src/Github.js
+++ src/Github.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react'
 
+import './Github.css'
+
 
 class Github extends Component {
+
+state={
+    username: '',
+}
+
+handleChange=(ev)=>{
+    this.setState({username: ev.target.value})
+}
+
     render(){
         return(
             <div className="Github" >
-            Component
+            <img
+          src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png"
+          alt="GitHub"
+          className="logo"
+        />
+            <h1>Github!</h1>
+
+            <form>
+                <div>
+                        <input type="text"
+                        required autoFocus 
+                        name="name"
+                        onChange={this.handleChange}
+                        value={this.state.username} />
+                    </div>
+
+                    <div>
+                            <button type="submit">
+                            Look Up User
+
+                            </button>
+                        </div>
+
+                        </form>
+
+
             </div>
         )
     }
